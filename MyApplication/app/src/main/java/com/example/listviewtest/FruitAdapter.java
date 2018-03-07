9785dace21427f170b0aedebf146086d9ef3b020
EDG-Zola <wellzengwei@163.com>
2018-03-07 17:08:30 +0800

    use convertView and ViewHolder to optimize performance

--- MyApplication/app/src/main/java/com/example/listviewtest/FruitAdapter.java
+++ MyApplication/app/src/main/java/com/example/listviewtest/FruitAdapter.java
@@ -27,14 +27,31 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
         Fruit fruit = getItem(position);//获取当前项的Fruit实例
         //只让父布局中声明的layout属性生效，而不为这个
         //View布局添加父布局，因为一旦添加父布局，就不能添加到ListView中了
-        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
-        //用findViewById()方法获取ImageView和TextView的实例，然后调用setImageResource()和setText()方法来显示图片和文字
-        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
-        TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
-        fruitImage.setImageResource(fruit.getImageId());
-        fruitName.setText(fruit.getName());
+        View view;
+        ViewHolder viewHolder;
+        //使用convertView参数将已经加载好的布局进行缓存，以提高ListView的运行效率
+        //在快速滚动页面时也能有较好地性能
+        if (convertView == null){
+            view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+            //用findViewById()方法获取ImageView和TextView的实例，然后调用setImageResource()和setText()方法来显示图片和文字
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = (TextView) view.findViewById(R.id.fruit_name);
+            view.setTag(viewHolder);//将ViewHolder存储在View中
+        }
+        else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();//重新获取ViewHolder
+        }
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
         //最后返回布局
         return view;
 
+    }
+    //新建一个内部类ViewHolder，用于对控件的实例进行缓存
+    class ViewHolder {
+        ImageView fruitImage;
+        TextView fruitName;
     }
 }
