e3ed3ca42cdbd3009b71d69a6621c909bcf2509f
EDG-Zola <wellzengwei@163.com>
2018-03-07 21:10:45 +0800

    creat new project RecyclerViewTest

---
+++ RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/Fruit.java
@@ -0,0 +1,27 @@
+package com.example.listviewtest;
+
+/**
+ * Created by Administrator on 2018\3\7 0007.
+ * 新建的一个Fruit类
+ * 成员变量：
+ * 水果名---name
+ * 水果图片Id号---imageId
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+
+    public Fruit(String name, int imageId){
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getImageId(){
+        return imageId;
+    }
+}
