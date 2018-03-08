817617021987aae72b2a73fab12442929c1fa902
EDG-Zola <wellzengwei@163.com>
2018-03-08 15:44:08 +0800

    try StaggeredGridLayoutManager

--- RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/MainActivity.java
+++ RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.recyclerviewtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
+import java.util.Random;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,8 +25,9 @@ public class MainActivity extends AppCompatActivity {
         //****在ArrayAdapter的构造函数中依次传入当前上下文，ListView子项布局的id，以及要适配的数据
         initFruits();//初始化水果数据
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycle_view);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
         FruitAdapter adapter = new FruitAdapter(fruitList);
         recyclerView.setAdapter(adapter);
@@ -40,28 +44,37 @@ public class MainActivity extends AppCompatActivity {
         /**** 在Fruits类的构造函数中将水果的名字和对应的图片Id传入
          * 然后将构建好的对象添加到水果列表fruitList中****/
         for (int i = 0; i < 2; i++) {
-            Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
+            Fruit apple = new Fruit(getRandomLengthName("Apple"), R.drawable.apple_pic);
             fruitList.add(apple);
-            Fruit banana = new Fruit("Banana", R.drawable.banana_pic);
+            Fruit banana = new Fruit(getRandomLengthName("Banana"), R.drawable.banana_pic);
             fruitList.add(banana);
-            Fruit orange = new Fruit("Orange", R.drawable.orange_pic);
+            Fruit orange = new Fruit(getRandomLengthName("Orange"), R.drawable.orange_pic);
             fruitList.add(orange);
-            Fruit watermelon = new Fruit("Watermelon", R.drawable.watermelon_pic);
+            Fruit watermelon = new Fruit(getRandomLengthName("Watermelon"), R.drawable.watermelon_pic);
             fruitList.add(watermelon);
-            Fruit pear = new Fruit("Pear", R.drawable.pear_pic);
+            Fruit pear = new Fruit(getRandomLengthName("Pear"), R.drawable.pear_pic);
             fruitList.add(pear);
-            Fruit grape = new Fruit("Grape", R.drawable.grape_pic);
+            Fruit grape = new Fruit(getRandomLengthName("Grape"), R.drawable.grape_pic);
             fruitList.add(grape);
-            Fruit pineapple = new Fruit("Pineapple", R.drawable.pineapple_pic);
+            Fruit pineapple = new Fruit(getRandomLengthName("Pineapple"), R.drawable.pineapple_pic);
             fruitList.add(pineapple);
-            Fruit strawberry = new Fruit("Strawberry", R.drawable.strawberry_pic);
+            Fruit strawberry = new Fruit(getRandomLengthName("Strawberry"), R.drawable.strawberry_pic);
             fruitList.add(strawberry);
-            Fruit cherry = new Fruit("Cherry", R.drawable.cherry_pic);
+            Fruit cherry = new Fruit(getRandomLengthName("Cherry"), R.drawable.cherry_pic);
             fruitList.add(cherry);
-            Fruit mango = new Fruit("Mango", R.drawable.mango_pic);
+            Fruit mango = new Fruit(getRandomLengthName("Mango"), R.drawable.mango_pic);
             fruitList.add(mango);
 
 
         }
+    }
+    private String getRandomLengthName(String name){
+        Random random = new Random();
+        int length = random.nextInt(20) + 1;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 20; i++){
+            builder.append(name);
+        }
+        return builder.toString();
     }
 }
