bf8f88c50121e1edf33f3af92ae31fde6ef23218
EDG-Zola <wellzengwei@163.com>
2018-03-08 11:06:35 +0800

    horizental scrolling

--- RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/MainActivity.java
+++ RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         initFruits();//初始化水果数据
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycle_view);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
         recyclerView.setLayoutManager(layoutManager);
         FruitAdapter adapter = new FruitAdapter(fruitList);
         recyclerView.setAdapter(adapter);
