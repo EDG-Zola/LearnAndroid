59f36af55eed5c878b00143e51f978a9e8579fc0
EDG-Zola <wellzengwei@163.com>
2018-04-22 21:09:03 +0800

    利用OpenCV官方demo进行人脸检测

---
+++ OpenCV4Android/OpencvFaceDetection/app/src/main/java/com/example/cvlab/opencvfacedetection/OpencvClass.java
@@ -0,0 +1,9 @@
+package com.example.cvlab.opencvfacedetection;
+
+/**
+ * Created by Administrator on 2018\4\22 0022.
+ */
+
+public class OpencvClass {
+    public native static void facedetection(long addrRgba);
+}
