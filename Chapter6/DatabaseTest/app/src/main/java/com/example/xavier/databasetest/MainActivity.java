fe328041a55d164adc91a4fb6f080428fef91fee
EDG-Zola <wellzengwei@163.com>
2018-05-27 12:03:18 +0800

    使用Sqlite创建数据库和表

---
+++ Chapter6/DatabaseTest/app/src/main/java/com/example/xavier/databasetest/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.xavier.databasetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private MyDatabaseHelper dbHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dbHelper = new MyDatabaseHelper(this, "Bookstore.db", null, 1);//创建一个数据库Bookstore.db，版本为1
+        dbHelper = new MyDatabaseHelper(this, "Bookstore.db", null, 2);//创建一个数据库Bookstore.db，版本为2
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbHelper.getWritableDatabase();
+            }
+        });
+    }
+}
---
+++ Chapter6/DatabaseTest/app/src/main/java/com/example/xavier/databasetest/MyDatabaseHelper.java
@@ -0,0 +1,45 @@
+package com.example.xavier.databasetest;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2018\5\27 0027.
+ * 继承SQLiteOpenHelper,用于创建一个数据库和表
+ */
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+    private static final String CREATE_BOOK = "create table Book("
+            + "id integer primary key autoincrement, "
+            + "author text, "
+            + "price real, "
+            + "pages integer, "
+            + "name text)";
+
+    private static final String CREATE_CATEGORY = "create table Category("
+            + "id integer primary key autoincrement, "
+            + "category_name text, "
+            + "category_code integer)";
+
+    private Context mContext;
+    public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_BOOK);//执行创建数据库的命令
+        db.execSQL(CREATE_CATEGORY);
+        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists Book");
+        db.execSQL("drop table if exists Category");
+        onCreate(db);
+    }
+}
